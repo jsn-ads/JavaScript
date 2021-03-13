216c164ec6654f9a14515ff3569ea3f416a3c6f7
neto <jsn.ads@gmail.com>
2021-03-12 21:26:59 -0300

    01-01 concluido

---
+++ 01-Introducao/01-Formas-de-mostrar/script.js
@@ -0,0 +1,7 @@
+document.getElementById("titulo").innerHTML = "O texto foi trocado";
+
+document.write("Adicionando um texto");
+
+window.alert("Usando a janela no navegador");
+
+console.log("A mensagem que eu inserir aqui, aparecerá no console.");
