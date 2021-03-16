a908bf291b4f6a2c74f186ea2ba1abddd29a3650
neto <jsn.ads@gmail.com>
2021-03-16 13:27:50 -0300

    02-03 Concluido

---
+++ 02-Conceitos-Basicos/03-GetElementById-innerHTML/script.js
@@ -0,0 +1,9 @@
+// document.getElementById("titulo").innerHTML = "Texto Trocado";
+
+var titulo = document.getElementById("titulo");
+
+titulo.innerHTML = "Segundo Titulo";
+
+var campo = document.getElementById("campo");
+
+campo.value = "Digite o seu nome";
