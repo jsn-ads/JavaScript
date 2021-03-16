876dd952d2d4da6c1eb2adee84027416098b0864
neto <jsn.ads@gmail.com>
2021-03-16 20:24:19 -0300

    02-04 Concluido

---
+++ 02-Conceitos-Basicos/04-Funcoes/script.js
@@ -0,0 +1,9 @@
+var titulo = document.getElementById("titulo");
+var texto = document.getElementById("texto");
+
+function alterar(){
+    titulo.innerHTML = "Trocou titulo";
+    texto.value = "Insira seu nome";
+}
+
+alterar();
