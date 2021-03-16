dcb435afe567b8cd1881a7cc42605c53cbf28efa
neto <jsn.ads@gmail.com>
2021-03-15 23:13:59 -0300

    01-02 Concluido

--- 01-Introducao/02-Variaveis/script.js
+++ 01-Introducao/02-Variaveis/script.js
@@ -11,3 +11,14 @@ var y = 15;
 var total = x + y;
 
 alert(nomeCompleto);
+
+let nome2 = "Cristina"; //usado somente dentro do escopo, apos se executado a memoria e limpa
+
+const nome3 = "Natalia"; //nao modifica 
+
+const pessoa = {
+        nome:'Jose', 
+        sobrenome:'Neto'
+    };
+
+console.log(pessoa);
