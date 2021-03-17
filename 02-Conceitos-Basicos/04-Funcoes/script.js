8fc417dffd9226360b307684749693486a71efc3
neto <jsn.ads@gmail.com>
2021-03-16 22:25:38 -0300

    02-04 Concluido

--- 02-Conceitos-Basicos/04-Funcoes/script.js
+++ 02-Conceitos-Basicos/04-Funcoes/script.js
@@ -6,4 +6,23 @@ function alterar(){
     texto.value = "Insira seu nome";
 }
 
+function somar(x, y){
+    let total = x + y;
+    document.write(total);
+}
+
+function multiplicar(x,y){
+
+    let total = x * y;
+
+    return total;
+
+}
+
 alterar();
+
+somar(4,3);
+
+var resultado = multiplicar(10,10);
+
+alert(resultado);
