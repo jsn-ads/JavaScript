47ded27c250df7bbe6ee16b9180b1d042c9c5b5f
neto <jsn.ads@gmail.com>
2021-03-20 01:13:16 -0300

    02-09 Concluido

---
+++ 02-Conceitos-Basicos/09-Objeto/script.js
@@ -0,0 +1,13 @@
+let animal = {
+    tipo:   "Cachorro",
+    raca:   "vira-lata",
+    peso:   "40 kg",
+    idade:  3,
+    barulho:function(){
+        console.log("uau uau uau uau...");
+    }
+    
+}
+
+console.log(animal.tipo);
+animal.barulho();
