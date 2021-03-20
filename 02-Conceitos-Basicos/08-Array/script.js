fd3039c94d26f60f89f898060ea475096e3ff593
neto <jsn.ads@gmail.com>
2021-03-19 22:19:50 -0300

    02-08 Concluido

---
+++ 02-Conceitos-Basicos/08-Array/script.js
@@ -0,0 +1,30 @@
+let carros = ['Palio', 'Uno', 'Corolla', 'Ferrari'];
+
+let games = new Array("ps4", "xbox", "3ds");
+
+let tv = [
+    "samsung",
+    "LG",
+    "Sony",
+    function(){
+        console.log("sony saiu do brasil");
+    }
+]
+
+tv[3]();
+
+console.log(carros);
+console.log(games);
+
+
+//mostra na posicao do array
+console.log(carros[1]);
+
+let listaArray = [
+    ["ps4", "xbox", "3ds"],
+    ["samsung", "lg", "sony"]
+];
+
+console.log(listaArray);
+
+console.log(listaArray[1][0]);
