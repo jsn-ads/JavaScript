7299f609ea973a81a5f8e61ce9eb207ccbefd36d
neto <jsn.ads@gmail.com>
2021-03-20 21:37:57 -0300

    02-09 Concluido

--- 02-Conceitos-Basicos/09-Objeto/script.js
+++ 02-Conceitos-Basicos/09-Objeto/script.js
@@ -7,7 +7,19 @@ let animal = {
         console.log("uau uau uau uau...");
     }
     
-}
+};
 
 console.log(animal.tipo);
 animal.barulho();
+
+// array de objetos
+
+let Animais = [
+    {tipo:"Cachorro", raca:"pitbull"},
+    {tipo:"Gato", raca:"siames"},
+    {tipo:"Porco-da-India", raca:"PeloFofo"},
+    {tipo:"Papagaio", raca:"Zefalador"}
+]
+
+console.log(Animais);
+console.log(Animais[2].raca+" "+Animais[2].tipo);
