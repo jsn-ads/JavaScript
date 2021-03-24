bb287f8065ed4d6fda62c1d1e810d9c89ed51a23
neto <jsn.ads@gmail.com>
2021-03-24 01:10:07 -0300

    03-01 70%

--- 03-Manipulacoes-Arrow-Ajax/01-Metodos-de-String/script.js
+++ 03-Manipulacoes-Arrow-Ajax/01-Metodos-de-String/script.js
@@ -1,18 +1,33 @@
 //Contar quantidade de caracteres em uma string
 let nome = "Jose Alves de Souza Neto";
 
-console.log("total: "+nome.length);
+    console.log("total: "+nome.length);
 
 //Verifica se possui tal caracter e em qual posição se encontra no array 
 
-console.log(nome.indexOf('Souza'));
+    console.log(nome.indexOf('Souza'));
 
-console.log(nome.indexOf('J'));
+    console.log(nome.indexOf('J'));
 
-console.log(nome.indexOf('Cristina'));
+    console.log(nome.indexOf('Cristina'));
 
-if(nome.indexOf('Jose') > -1){
-    console.log("Usuario encontrado");
-}else{
-    console.log("Usuario nao encontrado");
-}
+    if(nome.indexOf('Jose') > -1){
+        console.log("Usuario encontrado");
+    }else{
+        console.log("Usuario nao encontrado");
+    }
+
+//Pega o valor da posicao X a posicao Y conforme o exemplo 
+
+    console.log(nome.slice(10,18));
+    console.log(nome.slice(10));
+    console.log(nome.slice(-4));
+
+//faz a mesma coisa que slice porem mais limitado
+
+    console.log(nome.substring(10,18));
+
+//faz o que slice e substring faz e mais completo ,1 parametro( pega da onde quer comecar) 2 paramento(comeca a contar da foi marcado 1 parametro)
+
+    console.log(nome.substr(5,3));
+    
