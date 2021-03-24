5401aca7612d8b7f48972051df8ca5024b2f97aa
neto <jsn.ads@gmail.com>
2021-03-24 10:00:19 -0300

    03-01 Concluido

--- 03-Manipulacoes-Arrow-Ajax/01-Metodos-de-String/script.js
+++ 03-Manipulacoes-Arrow-Ajax/01-Metodos-de-String/script.js
@@ -29,5 +29,41 @@ let nome = "Jose Alves de Souza Neto";
 
 //faz o que slice e substring faz e mais completo ,1 parametro( pega da onde quer comecar) 2 paramento(comeca a contar da foi marcado 1 parametro)
 
-    console.log(nome.substr(5,3));
+    console.log(nome.substr(5,3)); 
+
+//Trocando valores de uma String 
+
+    let nomeAlterado = nome.replace("Alves de Souza", "Super");
+
+    console.log(nomeAlterado);
+
+//Caixa Alta e Caixa Baixa
+
+    let n1 = nome.toUpperCase();
+
+    console.log(n1);
+
+    let n2 = nome.toLowerCase();
+
+    console.log(n2);
+
+//Retirar espaçamento 
+
+    let e1 = "    Jose Neto  ";
+    console.log(e1.trim());
+
+//faz mesma funcao da substring 
+
+    let c1 = "Neto";
+
+    console.log(c1.charAt(2));
+
+    console.log(c1[2]);
+
+//Trasnforma uma string e uma array
+
+    let lista = nome.split(" "); // " ", "," , "."
+
+    console.log(lista);
+
     
