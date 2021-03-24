cd227edc075c20ce7e2f6724aa47101aba341614
neto <jsn.ads@gmail.com>
2021-03-24 00:13:18 -0300

    03-01 Concluido

---
+++ 03-Manipulacoes-Arrow-Ajax/01-Metodos-de-String/script.js
@@ -0,0 +1,18 @@
+//Contar quantidade de caracteres em uma string
+let nome = "Jose Alves de Souza Neto";
+
+console.log("total: "+nome.length);
+
+//Verifica se possui tal caracter e em qual posição se encontra no array 
+
+console.log(nome.indexOf('Souza'));
+
+console.log(nome.indexOf('N'));
+
+console.log(nome.indexOf('Cristina'));
+
+if(nome.indexOf('Jose') > -1){
+    console.log("Usuario encontrado");
+}else{
+    console.log("Usuario nao encontrado");
+}
