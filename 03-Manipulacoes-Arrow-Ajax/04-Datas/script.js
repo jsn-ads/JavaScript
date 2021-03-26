bb7b3bfcfa4331be7cc4c11d1dd0c63647cd1657
neto <jsn.ads@gmail.com>
2021-03-25 22:42:47 -0300

    03-04 70%

--- 03-Manipulacoes-Arrow-Ajax/04-Datas/script.js
+++ 03-Manipulacoes-Arrow-Ajax/04-Datas/script.js
@@ -6,8 +6,27 @@ console.log(d.toDateString());
 
 console.log(d.toUTCString());
 
-// definindo uma hora
+// definindo a hora
 
 let e = new Date('2020-01-01 15:42:17');
 
 console.log(e);
+
+// ano
+
+console.log(d.getFullYear());
+
+// mes
+console.log(d.getMonth());
+
+// dia
+
+console.log(d.getDate());
+
+// horas
+
+console.log(d.getHours());
+
+// milesegundos de 1970
+
+console.log(d.getTime());
