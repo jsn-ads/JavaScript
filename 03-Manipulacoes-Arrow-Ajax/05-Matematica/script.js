604f029dff831ea452acd4466c94c9e85c80351e
neto <jsn.ads@gmail.com>
2021-03-27 13:53:51 -0300

    03-05 Concluido

---
+++ 03-Manipulacoes-Arrow-Ajax/05-Matematica/script.js
@@ -0,0 +1,51 @@
+let x;
+
+// valor de PI
+
+x = Math.PI;
+
+console.log(x);
+
+// Arredonda o valor pra cima ou para baixo aparti do valor do meio
+
+x = Math.round(3.52);
+
+console.log(x);
+
+// sempre arredonda para baixo
+
+x = Math.floor(3.89);
+
+console.log(x);
+
+// sempre arredonda para cima 
+
+x = Math.ceil(3.21);
+
+console.log(x);
+
+// retorna o positivo
+
+x = Math.abs(-7.839);
+
+console.log(x);
+
+// retorna o menor numero da lista
+
+x = Math.min(8,673,77,202, 3);
+
+console.log(x);
+
+// retorna o maior numero da lista
+
+x = Math.max(8,673,77,202, 3);
+
+console.log(x);
+
+// retorna um numero aleatorio 
+
+x = Math.floor(Math.random() * 100);
+
+console.log(x);
+
+
