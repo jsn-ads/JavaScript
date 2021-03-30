d0dea8ec04f03d4fced6ef67ae7544e18c6c6a48
neto <jsn.ads@gmail.com>
2021-03-30 08:55:07 -0300

    03-08 2 Concluido

--- 03-Manipulacoes-Arrow-Ajax/08-Descontruindo-Objetos/script.js
+++ 03-Manipulacoes-Arrow-Ajax/08-Descontruindo-Objetos/script.js
@@ -12,9 +12,17 @@ let objeto = {
 };
 
 
-let {nome, sobreNome, idade} = objeto;
+let {nome, sobreNome, idade, social:{social1:redesocial}} = objeto;
 
-console.log(nome , sobreNome, idade);
+console.log(nome , sobreNome, idade, redesocial);
+
+//desconstruindo numa funcao 
+
+function nomeCompleto({nome, sobreNome, social:{social1:redesocial}}){
+    return `${nome} ${sobreNome} ${redesocial}`;
+}
+
+console.log(nomeCompleto(objeto));
 
 //caso queria definir nome de variavel ou deixa um valor definido caso nao tenha valor
 
