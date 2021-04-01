5e16b590a4c94b0df4d73b6cdee38b906fc7b546
neto <jsn.ads@gmail.com>
2021-04-01 18:51:17 -0300

    03-13 Concluido

---
+++ 03-Manipulacoes-Arrow-Ajax/13-Includes-e-Repeat/script.js
@@ -0,0 +1,16 @@
+let lista = [
+    "PHP",
+    "JAVA",
+    "JAVASCRIPT",
+    "SQL"
+]
+
+console.log(lista.includes("PHP"));
+
+console.log(lista.includes("HTML"));
+
+let nome = "PHP";
+
+console.log(nome.includes("H"));
+
+document.querySelector('.squad').innerHTML = "<div style='width:100px; height:100px; background-color:red; margin:5px;'></div>".repeat(3);
