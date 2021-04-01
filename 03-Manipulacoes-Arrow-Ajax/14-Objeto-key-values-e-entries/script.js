7a05e6221cd991f9dad6f7d92a6f1cedfbb2b206
neto <jsn.ads@gmail.com>
2021-04-01 19:37:20 -0300

    03-14 Concluido

---
+++ 03-Manipulacoes-Arrow-Ajax/14-Objeto-key-values-e-entries/script.js
@@ -0,0 +1,14 @@
+let objeto = {
+    nome: "Jose",
+    sobreNome: "Neto",
+    idade: 31
+}
+
+console.log("tipo: "+typeof objeto);
+
+console.log(Object.keys(objeto));
+
+console.log(Object.values(objeto));
+
+console.log(Object.entries(objeto));
+
