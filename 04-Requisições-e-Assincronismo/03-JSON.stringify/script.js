898993ac87dde819107fde69796fbd88383bae12
Work <jsn.ads@gmail.com>
2021-05-20 20:09:34 -0300

    04-03 Concluido

---
+++ 04-Requisições-e-Assincronismo/03-JSON.stringify/script.js
@@ -0,0 +1,11 @@
+let pessoa = {
+    nome:'José Alves', 
+    idade: 90,
+    caracteristicas:['sorridente','maravilhoso','top'],
+    estetica:{
+        peso: 90,
+        altura: 180
+    }
+}
+
+let JsonString = JSON.stringify(pessoa);
