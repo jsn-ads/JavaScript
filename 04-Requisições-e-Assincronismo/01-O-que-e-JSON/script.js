38a47102979851fb22bda9c328d830212dacec0c
Work <jsn.ads@gmail.com>
2021-05-20 19:27:14 -0300

    04-01 Concluido

---
+++ 04-Requisições-e-Assincronismo/01-O-que-e-JSON/script.js
@@ -0,0 +1,17 @@
+
+let estetica = {
+    peso: 1,
+    altura: 2
+}
+
+let pessoa = {
+        nome:'José Alves', 
+        idade: 90,
+        caracteristicas:['sorridente','maravilhoso','top'],
+        estetica:{
+            peso: 90,
+            altura: 180
+        }
+    }
+
+    console.log(pessoa);
