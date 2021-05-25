60ae6cae300ddea881fec81098bea4294f28c136
Work <jsn.ads@gmail.com>
2021-05-25 18:24:32 -0300

    04-04 Concluido

---
+++ 04-Requisições-e-Assincronismo/04-Callbacks/script.js
@@ -0,0 +1,12 @@
+function alertar(){
+    console.log('carregando dados');
+}
+
+let nome = "Jose";
+let sobrenome = "Neto";
+
+console.log(nome);
+
+setTimeout(alertar, 2000);
+
+console.log(sobrenome);
