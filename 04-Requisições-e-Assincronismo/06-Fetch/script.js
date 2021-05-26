04bb6d7a31f12822aa03ee55b5d0960e70852232
Work <jsn.ads@gmail.com>
2021-05-26 18:19:10 -0300

    04-06 Concluido

--- 04-Requisições-e-Assincronismo/06-Fetch/script.js
+++ 04-Requisições-e-Assincronismo/06-Fetch/script.js
@@ -2,15 +2,29 @@ function carregarPosts(){
 
     document.getElementById('posts').innerHTML = "Carregando...";
 
-
     fetch('https://jsonplaceholder.typicode.com/posts')
         .then(function(resultado){  
             return resultado.json();
         })
         .then(function(json){
-            document.getElementById('posts').innerHTML = json.length+' posts';
+            criarLista(json);
         })
         .catch(function(error){
             console.log("Erros ao carregar a lista");
         })
+}
+
+function criarLista(lista){
+
+    let html = '';
+
+    for(let i in lista){
+
+        html += "<h3>"+lista[i].title+"</h3>";
+        html += lista[i].body;
+        html += "<hr>";
+    }
+
+    document.getElementById('posts').innerHTML = html;
+
 }
