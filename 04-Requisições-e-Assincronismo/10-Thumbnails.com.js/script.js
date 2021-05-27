8320c290ad403c0fd2e2d649ebfaa2ff40ad6295
Work <jsn.ads@gmail.com>
2021-05-27 19:29:45 -0300

    04-11 Concluido

--- 04-Requisições-e-Assincronismo/10-Thumbnails.com.js/script.js
+++ 04-Requisições-e-Assincronismo/10-Thumbnails.com.js/script.js
@@ -1,10 +1,14 @@
 function gerar(){
-
+    let reader = new FileReader();
     let imagem = document.getElementById('imagem').files[0];
 
-    let img = document.createElement('img');
-    img.src = URL.createObjectURL(imagem);
-    img.width = 200;
+    reader.onloadend = function(){
+        let img = document.createElement('img');
+        img.src = reader.result;
+        img.width = 200;
 
-    document.getElementById("area").appendChild(img);
+        document.getElementById('area').appendChild(img);
+    }
+
+    reader.readAsDataURL(imagem);
 }
