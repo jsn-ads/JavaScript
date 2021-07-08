11ba900f112cda1aa17f8a4f45e22306830fdada
Neto <jsn.ads@gmail.com>
2021-07-07 21:48:00 -0300

    projeto concluido

--- Udemy/04-Projeto/js/scripts.js
+++ Udemy/04-Projeto/js/scripts.js
@@ -11,6 +11,7 @@ document.getElementById('sliding-header-menu-close-button').onclick = function()
 
 // About us Tab
 
+
 var aboutUs = {
   "Missão": "Fazer com que cada cliente seja reconhecido como autoridade em seu segmento de atuação. Agregar valor ao negócio, potencializar o crescimento das operações e promover e estreitar o relacionamento do cliente com o seu público alvo, por meio da geração de conteúdo de relevância.",
   "Visão": "Ser reconhecida pelos clientes e pelo mercado como uma empresa parceira, inovadora e criativa, que oferece sempre os melhores produtos e soluções em Comunicação Empresarial Integrada.",
@@ -19,6 +20,28 @@ var aboutUs = {
 
 var unseletected_color = "#646872";
 var seletected_color = "#2A2D34";
+
+let about_tags = document.getElementsByClassName('single-tab');
+
+for (let i = 0; i < about_tags.length; i++) {
+    about_tags[i].onclick = function(){
+     
+      for (let j = 0; j < about_tags.length; j++) { 
+          about_tags[j].style['background-color'] = unseletected_color;
+          about_tags[j].style['font-weight'] = "normal";
+      }
+
+      this.style['background-color'] = seletected_color;
+      this.style['font-weight'] = 'bold';
+
+      let selecionado = this.innerHTML;
+
+      document.getElementById('box-text').innerHTML = aboutUs[selecionado];
+
+    }
+}
+
+
 
 
 
@@ -42,6 +65,32 @@ var our_services = [
   
 ];
 
+
+let servico_atual = 0;
+
+document.getElementById("service-previous").onclick = function(){
+  if(servico_atual == 0){
+    var servico_anterior = our_services.length - 1;
+  }else{
+    var servico_anterior = servico_atual - 1;
+  }
+
+  document.getElementById("service-title").innerHTML = our_services[servico_anterior].title;
+  document.getElementById("service-text").innerHTML = our_services[servico_anterior].text;
+  servico_atual = servico_anterior;
+}
+
+document.getElementById("service-next").onclick = function(){
+  if(servico_atual == our_services.length - 1){
+    var servico_seguinte = 0;
+  }else{
+    var servico_seguinte = servico_atual + 1;
+  }
+
+  document.getElementById("service-title").innerHTML = our_services[servico_seguinte].title;
+  document.getElementById("service-text").innerHTML = our_services[servico_seguinte].text;
+  servico_atual = servico_seguinte;
+}
 
 // Data Footer
 
