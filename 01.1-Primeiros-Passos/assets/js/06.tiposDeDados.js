2dfaa327144e2fa28323cc41bd85f50d118bea20
Neto Evtek <jsn.ads@gmail.com>
2023-09-01 15:32:54 -0300

    Primeiros Passos - Tipos de dados

---
+++ 01.1-Primeiros-Passos/assets/js/06.tiposDeDados.js
@@ -0,0 +1,11 @@
+let nome = "Jose Neto"                      //String
+let idade = 30                              //Number
+let status = true                           //Boolean 
+let lista = ["texto","texto2","texto3"];    //Array
+let pessoa = {nome:"Neto", idade: 33}       //Objeto
+
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof status);
+console.log(typeof lista);
+console.log(typeof pessoa);
