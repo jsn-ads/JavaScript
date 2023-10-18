a72cec48d5352c9bdf5d4366e285d6fafc29892c
Neto Evtek <jsn.ads@gmail.com>
2023-10-18 18:05:21 -0300

    Primeiros Passos - Template String

---
+++ 01.1-Primeiros-Passos/assets/js/11.TemplateString.js
@@ -0,0 +1,14 @@
+let nome = "jose";
+let sobrenome = "Neto";
+let idade = 90;
+
+// let nomeCompleto = nome + " " + sobrenome;
+
+let nomeCompleto = `${nome} ${sobrenome}`;
+
+console.log(nomeCompleto);
+
+let idadeString = `${idade} anos`;
+
+console.log(idadeString);
+
