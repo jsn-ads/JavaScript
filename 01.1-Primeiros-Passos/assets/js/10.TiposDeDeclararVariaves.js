f03289772fd2de1dd0f07d07d8623bc4e7bd88d4
Neto Evtek <jsn.ads@gmail.com>
2023-10-18 16:39:43 -0300

    Primeros Passos - Operaçoes em variaveis

---
+++ 01.1-Primeiros-Passos/assets/js/10.TiposDeDeclararVariaves.js
@@ -0,0 +1,9 @@
+let nome = "neto"; //utiliza no contexto criado
+
+var nome2 = "neto2"; //utilizada em qualquer momento
+
+const nome3 = "neto3"; //valor fixo não pode ser alterado
+
+console.log(nome);
+console.log(nome2);
+console.log(nome3);
