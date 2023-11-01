50dc807e16a2bd948da26edc69ef32cdff825fe7
Neto Evtek <jsn.ads@gmail.com>
2023-11-01 09:28:18 -0300

    Primeiros Passos - Condicional

---
+++ 01.1-Primeiros-Passos/assets/js/13.condicional.js
@@ -0,0 +1,17 @@
+let idade = 14;
+
+if(idade > 17)
+{
+    console.log('voce e maior de idade');
+}
+else
+{
+    console.log('vc e menor de idade');
+}
+
+// >   Maior
+// <   Menor
+// ==  Igual
+// >=  Maior igual
+// <=  Menor Igual
+// !=  Diferente
