e6772f031a8f6d1e8d192f961a617adfed073ecf
Neto Home <jsn.ads@gmail.com>
2023-12-27 20:41:30 -0300

    Primeiros Passos - Condicional Ternario

---
+++ 01.1-Primeiros-Passos/assets/js/19.condicionalTernario.js
@@ -0,0 +1,13 @@
+
+let isMember = true;
+
+let shipping = isMember ? 2 : 10;
+
+console.log(isMember ? 'Você é membro' : 'Você não e membro');
+console.log(shipping);
+
+let age = 90;
+
+let isAdult = ((age > 14 && age < 60) ? 'sim' : 'não');
+
+console.log(isAdult);
