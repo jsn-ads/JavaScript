92a062faa11e70dbc46c4246eb5d166455113776
Neto Evtek <jsn.ads@gmail.com>
2024-09-11 17:00:19 -0300

    Js Basico - Arrow Function

---
+++ 02.1-Javascript Básico/07.Arrow Function/script.js
@@ -0,0 +1,29 @@
+//padrao
+
+function somar(x,y){
+    return x + y
+}
+
+console.log(somar(12,34))
+
+//Arrow Function 
+
+somar2 = (x,y) =>{
+    return x + y;
+}
+
+console.log(somar2(12,12))
+
+// Arrow Function reduzido
+
+somar3 = (x,y) => x+y;
+
+console.log(somar3(6,6))
+
+// Arrow Function unica variavel
+
+sob = s => "Jose " + s;
+
+console.log(sob("Neto"))
+
+
