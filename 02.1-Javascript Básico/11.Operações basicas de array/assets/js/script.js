daf4cd48c55274280e5f8e2777bd94a9bd5913a5
Neto Home <jsn.ads@gmail.com>
2024-09-27 08:43:49 -0300

    Js Basico - Operações basicas de array

--- 02.1-Javascript Básico/11.Operações basicas de array/assets/js/script.js
+++ 02.1-Javascript Básico/11.Operações basicas de array/assets/js/script.js
@@ -6,4 +6,15 @@ let array = [
     'dados 5'
 ]
 
+// adicionar item na lista
+array.push('dados 6');
+
+// remove o ultimo item da lista
+array.pop();
+
+//remove o primeiro item da lista
+array.shift();
+
+console.log(`lista : ${array}`);
+
 console.log(`Quantidade de dados: ${array.length}`);
