aa528ff4a3e4a4bb48c8c5f15185dc4f4298a281
Neto Home <jsn.ads@gmail.com>
2024-10-23 10:29:30 -0300

    Eventos e Manipulação de DOM - Selecionando elementos

--- 2.2-Eventos e manipulação de DOM/01.Selecionando Elementos/assets/js/script.js
+++ 2.2-Eventos e manipulação de DOM/01.Selecionando Elementos/assets/js/script.js
@@ -1,1 +1,9 @@
 document.getElementsByTagName("h1");
+
+document.getElementById("listaA");
+
+document.getElementsByClassName("botao");
+
+document.querySelector("#listaB");
+
+document.querySelectorAll("#listaA ul li");
