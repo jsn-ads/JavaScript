d357ef9bee9d26575663b343393145fd47ee1142
Neto Home <jsn.ads@gmail.com>
2024-11-06 22:22:12 -0300

    Eventos e Manipulação de DOM - Manipulando elementos

--- 2.2-Eventos e manipulação de DOM/03.Manipulando elementos/assets/js/script.js
+++ 2.2-Eventos e manipulação de DOM/03.Manipulando elementos/assets/js/script.js
@@ -1,7 +1,30 @@
 finish = () =>{
     const lista = document.querySelector("#lista");
     const ul = lista.querySelector('ul');
-    ul.innerHTML = "<li>Item Alterado</li>";
+
+    // // innerHTML = monidifica a estrutura do html subistituindo o elemento
+    // ul.innerHTML = "<li>Item Alterado</li>";
+
+    // // append = adiciona apenas String sem precisar remover o elemento da memoria
+    // ul.children[0].append(" (alterado)");
+
+    // // document.createElement = cria um elemento
+    // // appendChild = adiciona um elemento html no FINAL sem precisar remover elemento da memoria
+    // // prepend = adiciona um elemento html no INICIO sem precisar remover elemento da memoria
+    // let elemento = document.createElement("li");
+    // elemento.innerHTML = "Novo item";
+    // ul.appendChild(elemento);
+    // ul.prepend(elemento);
+
+    // // after = adiciona DEPOIS do elemento
+    // // before = adiciona ANTES do elemento
+    // let btn_1 = document.createElement("button");
+    // btn_1.innerHTML = "Finalizar ?";
+    // ul.after(btn_1);
+    // ul.before(btn_1);
+
+
+
 }
 
 
